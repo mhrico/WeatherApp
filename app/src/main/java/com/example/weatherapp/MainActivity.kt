e6b9638bc2c390fc62b7de868cb3f2f95e3c6737
Mehedi Hassan Rico <mhrico2@gmail.com>
2021-12-18 20:18:19 +0600

    feat: Get JSON weather data

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,14 +2,35 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val lat = intent.getStringExtra("lat")
-        val long = intent.getStringExtra("long")
-        Toast.makeText(this, lat + " " +long, Toast.LENGTH_LONG).show()
+        val lon = intent.getStringExtra("long")
+        getJsonData(lat, lon)
+    }
+
+    private fun getJsonData(lat: String?, lon: String?) {
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val apiKey: String = "5708ca44984af65e51ceb21553ff00c8"
+        val url = "api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}"
+
+// Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+
+            },
+            Response.ErrorListener{})
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 }
