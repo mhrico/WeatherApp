707925b0e5c47eb8963e2353afa28ab71c3b2f1c
Mehedi Hassan Rico <mhrico2@gmail.com>
2021-12-18 15:56:58 +0600

    feat: Get location permissions

--- app/src/main/java/com/example/weatherapp/SplashScreen.kt
+++ app/src/main/java/com/example/weatherapp/SplashScreen.kt
@@ -1,20 +1,101 @@
 package com.example.weatherapp
 
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.requestLocationUpdates
+import com.google.android.gms.location.*
 
 class SplashScreen : AppCompatActivity() {
+    lateinit var mfusedlocation:FusedLocationProviderClient
+    private var myRequestCode = 1010
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        },2000)
+        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
+        
+        getLastLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if(checkPermission()){
+            if(locationEnable()){
+                mfusedlocation.lastLocation.addOnCompleteListener{
+                    task ->
+                    var location: Location? = task.result
+                    if(location == null){
+                        newLocation()
+                    }else{
+                        Log.i("Location", location.longitude.toString())
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Please turn on your GPS location", Toast.LENGTH_LONG).show()
+            }
+            
+        } else{
+            requestPermission()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun newLocation() {
+        var locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates =  1
+        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
+        mfusedlocation.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+    }
+
+    private val locationCallback = object: LocationCallback(){
+        override fun onLocationResult(p0: LocationResult) {
+            var lastLocation:Location=p0.lastLocation
+        }
+    }
+
+    private fun locationEnable(): Boolean {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            myRequestCode
+        )
+    }
+
+    private fun checkPermission(): Boolean {
+        return (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == myRequestCode){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation()
+            }
+        }
     }
 }
